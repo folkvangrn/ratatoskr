f3c75145545dc7bcc805db20fbad95cae1c946a0
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-06-07 07:34:17 +0200

    Add routing

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/_index.scss';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <p>hi</p>
-  </React.StrictMode>
-);
