3bad813bd28317465b8ac3b07dea5e319df1d317
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-06-19 11:45:21 +0200

    Orders list and create form (#4)
    * Downgrade reactjs and install packages
    
    * Add an input component
    
    * Added order form and list

--- src/types/Order.ts
+++ src/types/Order.ts
@@ -1,13 +1,20 @@
 export type Order = {
   id?: number;
-  orderedAt: string;
+  orderedAt?: string;
   totalPrice: number;
   meals: Meal[];
 };
 
 export type Meal = {
-  id: number;
+  id?: number;
   name: string;
   price: number;
   availability: boolean;
+  ingredients?: Ingredient[] ;
+};
+
+export type Ingredient = {
+  id?: number;
+  name: string;
+  quantity: number;
 };
