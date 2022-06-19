6f62818e9a29fd2363ff6922b26cc2d608e844fd
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-06-19 11:42:44 +0200

    Downgrade reactjs and install packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import Root from './Root';
 import './styles/_index.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Root />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
